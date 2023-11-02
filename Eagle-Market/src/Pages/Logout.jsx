6b4489e2f43a215c4764bdc4bfe1c07a7916048b
IvanGM <ivangm_01@hotmail.com>
2023-11-02 02:04:29 -0600

    feat: Agregada opción de logout

---
+++ Eagle-Market/src/Pages/Logout.jsx
@@ -0,0 +1,23 @@
+import useAuthContext from '@/Context/useAuthContext'
+import { useEffect } from 'react'
+
+const Logout = () => {
+  const { setToken, setLoginStatus, setUserInfo } = useAuthContext()
+  // setToken('')
+  // setLoginStatus(false)
+  // setUserInfo({})
+  useEffect(() => {
+    sessionStorage.setItem('token', '')
+    sessionStorage.setItem('loginStatus', JSON.stringify(false))
+    sessionStorage.setItem('userInfo', JSON.stringify({}))
+    setToken('')
+    setLoginStatus(false)
+    setUserInfo({})
+  }, [setLoginStatus, setToken, setUserInfo])
+
+  return (
+    <div>Has cerrado sesión exitosamente</div>
+  )
+}
+
+export default Logout
