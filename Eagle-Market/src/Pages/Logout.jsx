09fb7fb450e90e9649715c5283a3d239169b14e3
IvanGM <ivangm_01@hotmail.com>
2023-11-02 22:26:01 -0600

    feat: Agregado Cart Context

--- Eagle-Market/src/Pages/Logout.jsx
+++ Eagle-Market/src/Pages/Logout.jsx
@@ -1,8 +1,10 @@
 import useAuthContext from '@/Context/AuthContext/useAuthContext'
+import useCartContext from '@/Context/CartContext/useCartContext'
 import { useEffect } from 'react'
 
 const Logout = () => {
   const { setToken, setLoginStatus, setUserInfo } = useAuthContext()
+  const { setCart } = useCartContext()
   // setToken('')
   // setLoginStatus(false)
   // setUserInfo({})
@@ -10,10 +12,12 @@ const Logout = () => {
     sessionStorage.setItem('token', '')
     sessionStorage.setItem('loginStatus', JSON.stringify(false))
     sessionStorage.setItem('userInfo', JSON.stringify({}))
+    sessionStorage.setItem('cart', JSON.stringify([]))
     setToken('')
     setLoginStatus(false)
     setUserInfo({})
-  }, [setLoginStatus, setToken, setUserInfo])
+    setCart([])
+  }, [setCart, setLoginStatus, setToken, setUserInfo])
 
   return (
     <div>Has cerrado sesión exitosamente</div>
