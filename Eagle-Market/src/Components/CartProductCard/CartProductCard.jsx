3367d980e2fd157d34945f43ba37eb53d757e0de
IvanGM <ivangm_01@hotmail.com>
2023-11-06 19:39:31 -0600

    feat: Modificada tarjeta para historial

--- Eagle-Market/src/Components/CartProductCard/CartProductCard.jsx
+++ Eagle-Market/src/Components/CartProductCard/CartProductCard.jsx
@@ -2,35 +2,50 @@
 // import useAuthContext from '@/Context/AuthContext/useAuthContext'
 import { useNavigate } from 'react-router-dom'
 
-const CartProductCard = ({ data, changeValueFunction, type, onDelete }) => {
+const nothingFunction = () => {}
+
+const CartProductCard = ({ data, changeValueFunction = nothingFunction, type = '', onDelete = nothingFunction, changeable = true }) => {
   const navigate = useNavigate()
   // const { userInfo } = useAuthContext()
   return (
-    <div className='card' style={{ width: '80%', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }} onClick={() => navigate(`/product/${data?.id}`)}>
+    <div
+      className='card' style={{ width: '80%', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }} onClick={(event) => {
+        event.stopPropagation()
+        navigate(`/product/${data?.id}`)
+      }}
+    >
       <p><strong>{data?.product_name}</strong></p>
-      <button onClick={(event) => {
-        event.stopPropagation()
-        changeValueFunction(type, data?.product_amount - 1, data?.id)
-      }}
-      >
-        -
-      </button>
+      {changeable
+        ? <button onClick={(event) => {
+          event.stopPropagation()
+          changeValueFunction(type, data?.product_amount - 1, data?.id)
+        }}
+          >
+          -
+          </button>
+        : ''}
       <p>Cantidad: {data.product_amount}</p>
-      <button onClick={(event) => {
-        event.stopPropagation()
-        changeValueFunction(type, data?.product_amount + 1, data?.id)
-      }}
-      >
-        +
-      </button>
+      {changeable
+        ? <button onClick={(event) => {
+          event.stopPropagation()
+          changeValueFunction(type, data?.product_amount + 1, data?.id)
+        }}
+          >
+          +
+        </button>
+        : ''}
+
       <p>Precio individual: ${data.price}</p>
       <p><strong>Total: ${data.price * data.product_amount}</strong></p>
-      <button onClick={(event) => {
-        event.stopPropagation()
-        onDelete()
-      }}
-      >X
-      </button>
+      {changeable
+        ? <button onClick={(event) => {
+          event.stopPropagation()
+          onDelete()
+        }}
+          >X
+        </button>
+        : ''}
+
     </div>
   )
 }
