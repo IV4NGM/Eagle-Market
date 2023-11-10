cb9f49bdd7fc69ea1e3dea2146f719b43082eb22
IvanGM <ivangm_01@hotmail.com>
2023-11-10 01:04:00 -0600

    feat: Añadidos estilos de tarjetas

--- Eagle-Market/src/Components/ProductCard/ProductCard.jsx
+++ Eagle-Market/src/Components/ProductCard/ProductCard.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/prop-types */
 // import useAuthContext from '@/Context/AuthContext/useAuthContext'
 import { useNavigate } from 'react-router-dom'
+import ProductDefaultImage from '@/assets/product-default-image.png'
 
 const ProductCard = ({ data }) => {
   const navigate = useNavigate()
   // const { userInfo } = useAuthContext()
   return (
-    <div className='card' style={{ width: '18rem' }} onClick={() => navigate(`/product/${data?.id}`)}>
-      <img src={data?.image || data?.base64Image} className='card-img-top' alt={data?.product_name} />
+    <div className='card product-card' onClick={() => navigate(`/product/${data?.id}`)}>
+      <img src={data?.image || data?.base64Image || ProductDefaultImage} className='card-img-top product-image-card' alt={data?.product_name} />
       <div className='card-body'>
         <h5 className='card-title'>{data?.product_name}</h5>
         <p className='card-text'>${data?.price}</p>
