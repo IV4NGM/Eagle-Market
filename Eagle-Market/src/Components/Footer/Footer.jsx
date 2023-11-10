b4eeddc59c18bb26028c92cb715d003999dd7186
IvanGM <ivangm_01@hotmail.com>
2023-11-10 01:03:23 -0600

    feat: Footer añadido

--- Eagle-Market/src/Components/Footer/Footer.jsx
+++ Eagle-Market/src/Components/Footer/Footer.jsx
@@ -1,6 +1,28 @@
+import { Link } from 'react-router-dom'
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined'
+import './Footer.scss'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <nav className='navbar bottom bg-body-tertiary footer'>
+        <div className='container-fluid footer-body'>
+          <p className='bold-text'>Eagle Market 2023</p>
+          {/* <div className='attribution'>
+          <img src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg' alt='TMDB logo' />
+          <p>This product uses the TMDB API but is not endorsed or certified by TMDB.</p>
+        </div> */}
+          <p>Designed by Iván G M</p>
+          <div className='mail-footer'>
+            <EmailOutlinedIcon />
+            <Link to='mailto:ivangm_01@hotmail.com'>
+              ivangm_01@hotmail.com
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </div>
+
   )
 }
 
